31dc59d127a48009a67b34b7b7be7fa3631a7def
Jeff Marrone <jgm1103@gmail.com>
2012-08-29 19:01:13 -0400

    End of Chapter 5 exercises

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,13 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+  
+  def logo(parameters)
+    logo_image = image_tag("logo.png", 
+    	:alt => parameters[:alt_text], 
+    	:class => "round"
+	);
+    
+    link_to logo_image, root_path
+  end
 end
